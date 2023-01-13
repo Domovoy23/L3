7af56cfd2cb448c1067547d17a1a4f846129d55f
Domovoy23 <89829210486a@gmail.com>
2023-01-13 18:22:58 +0500

    Conditional operators

---
+++ src/main/kotlin/7.kt
@@ -0,0 +1,11 @@
+import java.util.*
+fun main() {
+    println("Введите числo")
+    var num: Int = Scanner(System.`in`).nextInt()
+    if(num%2 ==0){
+        println("Чётное")
+    }
+    else
+        println("Не чётное")
+
+}
